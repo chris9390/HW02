0249a8901f4406396b89870216c0c3099f987990
Young-in Kim <chris9390@naver.com>
2018-03-28 13:33:26 +0900

    commit.

---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  FirstApp
+//ss
+//  Created by Release on 2018. 3. 21..
+//  Copyright © 2018년 Young-in Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    let messageArray = ["May the force be with you",
+                        "Live long and prosper",
+                        "To infinity and beyond",
+                        "Space is big. You just won't believe how vastly, hugely, mindbogglingly big it is",
+                        10] as [Any]
+    
+    var index = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func dobuttontap(_ sender: UIButton) {
+        print("Button Touched")
+        if let nextString = self.messageArray[index] as? String {
+            self.messageLabel.text = nextString
+        }
+        
+        index = index + 1
+        index %= self.messageArray.count
+    }
+
+}
+
